13305b2af7f0b7da08527053cf545663dba1d361
Victor Frette <victorafrette@gmail.com>
2024-12-09 20:22:27 -0300

    cambios 2

---
+++ codigo.js
@@ -0,0 +1,9 @@
+let num1 = parseInt(prompt (nombre + " Decime un numero para sumar "))
+let num2 = parseInt(prompt (nombre + " Decime Otro numero para sumar"))
+let resultado = num1+num2
+alert ("el resultado de tu operacion es " + resultado)
+
+let num3 = prompt (nombre + " Decime un numero para multiplicar")
+let num4 = prompt (nombre + " Decime Otro numero para multiplicar")
+let result = num3*num4
+alert ("el resultado de tu operacion es " + result)
